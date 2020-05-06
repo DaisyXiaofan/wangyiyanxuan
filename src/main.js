503bda0b3eb8dceb5851f8f9451c00115f30168a
xiaofan <email@example.com>
2020-05-06 17:12:06 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//注册路由
+import router from "@/router";
+//注册vuex
+import store from "@/store";
+
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
